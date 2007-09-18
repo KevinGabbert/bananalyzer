074f3b7395b9355e95b0b9b1277b8f67007e1ab9
kevingabbert <kevingabbert@26fb18f2-463a-0410-bc15-3755718e4027>
2007-09-18 06:10:18 +0000

    cleaning up

--- Bananalyzer/BananaPot/BananaADC.vb
+++ Bananalyzer/BananaPot/BananaADC.vb
@@ -1,6 +1,10 @@
 Public Class BananaADC
+
     Public Const AVREF As Double = 5
     Private _Active As Boolean = False
+
+#Region "Properties"
+
     Public Overrides Property Text() As String
         Get
             Return Me.ADC0Label.Text
@@ -23,11 +27,22 @@ Public Class BananaADC
             LED.Image = IIf(value, imgLEDs.Images(1), imgLEDs.Images(0))
         End Set
     End Property
+
+#End Region
+#Region "Event Handlers"
+
     Private Sub LED_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LED.Click
         _Active = Not _Active
         LED.Image = IIf(_Active, imgLEDs.Images(1), imgLEDs.Images(0))
     End Sub
+
+#End Region
+#Region "Functions"
+
     Public Sub SetVolts(ByVal ADCValue As UInt16)
         lblVolts.Text = Math.Round(ADCValue * (AVREF / 1023), 3).ToString("0.000") & " V"
     End Sub
+
+#End Region
+
 End Class
